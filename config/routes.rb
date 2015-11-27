8ae812ebfba603c6a4165e5b1c5d4d7fca3a6c53
wooxx074 <wooxx074@umn.edu>
2015-11-27 14:11:13 +0000

    Changes routing URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  #root page
+  root 'pages#index'
+  #Defines routes for Pages
+  get '/home' => 'pages#home' #goes straight to URL/home instead of URL/pages/home
+  get '/profile' => 'pages#profile'
+  get '/explore' => 'pages#explore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
